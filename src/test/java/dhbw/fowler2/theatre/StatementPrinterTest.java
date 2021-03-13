56d0928b7cc208e17e5920db1148e9b6c0fef0d2
klink.max-arthur <max.klink@gmx.de>
2021-03-13 14:01:05 +0100

    Fehler in der Ordnerstruktur: Verschiebung von StatementPrinterTest in den Testordner

--- src/test/java/dhbw/fowler2/theatre/StatementPrinterTest.java
+++ src/test/java/dhbw/fowler2/theatre/StatementPrinterTest.java
@@ -31,4 +31,28 @@ public class StatementPrinterTest {
                 "Amount owed is $1,730.00\n" +
                 "You earned 47 credits\n", result);
     }
+
+    @Test
+    public void printsHTMLStatements(){
+        Map<String, Play> plays = Map.of("hamlet", new Play("Hamlet", "tragedy"),
+                "as-like", new Play("As You Like It", "comedy"),
+                "othello", new Play("Othello", "tragedy"));
+
+        Invoice invoice = new Invoice("BigCo", List.of(new Performance("hamlet", 55),
+                new Performance("as-like", 35),
+                new Performance("othello", 40)));
+
+        StatementPrinter statementPrinter = new StatementPrinter();
+        String result = statementPrinter.printHTML(invoice, plays);
+
+        Assert.assertEquals("Statement print mismatch",
+                "<h1>Statement for BigCo</h1>\n" +
+                "<ul>\n" +
+                "  <li>Hamlet: $650.00 (55 seats)</li>\n" +
+                "  <li>As You Like It: $580.00 (35 seats)</li>\n" +
+                "  <li>Othello: $500.00 (40 seats)</li>\n" +
+                "</ul>\n"+
+                "<p>Amount owed is $1,730.00</p>\n" +
+                "<p>You earned 47 credits</p>\n", result);
+    }
 }
