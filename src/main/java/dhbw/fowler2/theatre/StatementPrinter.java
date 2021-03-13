16e44be9abace3cf009791342f3cb608b007e8a6
klink.max-arthur <max.klink@gmx.de>
2021-03-13 13:43:08 +0100

    Sprint 3: User Story 5 (Lila erledigt)

--- src/main/java/dhbw/fowler2/theatre/StatementPrinter.java
+++ src/main/java/dhbw/fowler2/theatre/StatementPrinter.java
@@ -5,14 +5,13 @@ import java.util.Locale;
 import java.util.Map;
 
 public class StatementPrinter {
-    PerformanceCalculator perfCalc;
+    private PerformanceCalculator perfCalc;
+    private NumberFormat format = NumberFormat.getCurrencyInstance(Locale.US);
 
     public String print(Invoice invoice, Map<String, Play> plays) {
         perfCalc = new PerformanceCalculator();
 
         var result = String.format("Statement for %s\n", invoice.customer);
-
-        NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
         for (var perf : invoice.performances) {
             var play = plays.get(perf.playID);
@@ -22,11 +21,34 @@ public class StatementPrinter {
             perfCalc.addVolumeCredits(perf.audience, play.type);
 
             // print line for this order
-            result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(perfCalc.addAmount(play, perf.audience) / 100), perf.audience);
+            result += String.format("  %s: %s (%s seats)\n", play.name, format.format(perfCalc.addAmount(play, perf.audience) / 100), perf.audience);
         }
-        result += String.format("Amount owed is %s\n", frmt.format(perfCalc.getTotalAmount() / 100));
+        result += String.format("Amount owed is %s\n", format.format(perfCalc.getTotalAmount() / 100));
         result += String.format("You earned %s credits\n", perfCalc.getVolumeCredits());
         return result;
+    }
+
+    public String printHTML(Invoice invoice, Map<String, Play> plays){
+        perfCalc = new PerformanceCalculator();
+
+        String htmlResult = String.format("<h1>Statement for "+invoice.customer+"</h1>\n");
+        htmlResult += "<ul>\n";
+
+        for(var perf : invoice.performances){
+            var play = plays.get(perf.playID);
+
+            // add volume credit
+            // add extra credit for every ten comedy attendees
+            perfCalc.addVolumeCredits(perf.audience, play.type);
+
+            // print line for this order
+            htmlResult += String.format("  <li>%s: %s (%s seats)</li>\n", play.name, format.format(perfCalc.addAmount(play, perf.audience) / 100), perf.audience);
+        }
+        htmlResult += "</ul>\n";
+        htmlResult += String.format("<p>Amount owed is %s</p>\n", format.format(perfCalc.getTotalAmount() / 100));
+        htmlResult += String.format("<p>You earned %s credits</p>\n", perfCalc.getVolumeCredits());
+
+        return htmlResult;
     }
 
 }
